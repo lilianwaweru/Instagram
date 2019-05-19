47177284bc36cd65e5d7f8cdbc1c55cddfad3aeb
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-20 02:50:47 +0300

    added an admin page

--- insta/models.py
+++ insta/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     name = models.CharField(max_length = 70)
-    caption = models.TextField()
+    description = models.TextField()
     image = models.ImageField(upload_to = 'images/',blank=True)
     
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()   
     
