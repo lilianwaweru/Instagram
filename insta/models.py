d32e323bd374de7a0b986caaee744b4a550b0fce
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-20 17:56:38 +0300

    added a user relationship in models.py

--- insta/models.py
+++ insta/models.py
@@ -1,9 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+    bio = models.CharField(max_length = 70)
+    profile_picture = models.ImageField(upload_to = 'images/',blank=True)
+    
+
+
+
+
 class Image(models.Model):
     name = models.CharField(max_length = 70)
+    profile = models.ForeignKey(Profile)
     image = models.ImageField(upload_to = 'images/',blank=True)
+    caption = models.TextField()
     
 
     def save_image(self):
@@ -18,6 +29,3 @@ class Image(models.Model):
         images = cls.objects.filter(image__name__icontains = search_term)
         return images
       
-class Profile(models.Model):
-    followers= models.IntegerField(default=0)
-    followi
