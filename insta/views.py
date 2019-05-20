a8b48bc1b2b11ead9dff6db1719686a8fd80b535
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-20 12:02:30 +0300

    defined search option in views.py

--- insta/views.py
+++ insta/views.py
@@ -6,5 +6,14 @@ def welcome(request):
     return render(request,'welcome.html')
 
 def search_image(request):
+
     if 'image' in request.GET and request.GET["image"]:
-        search_term = Image    
+        search_term = (request.GET.get("image")).title()
+        searched_images = Image.search_by_image(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'search.html',{"message":message})    
