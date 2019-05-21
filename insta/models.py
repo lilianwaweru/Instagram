9dfba8c19825adf1f6c5c7761228f16090b16798
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-21 11:49:33 +0300

    created forms.py

--- insta/models.py
+++ insta/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete = models.CASCADE)
+    infor = models.IntegerField(default=0)
     bio = models.CharField(max_length = 70)
     profile_picture = models.ImageField(upload_to = 'images/',blank=True)
 
