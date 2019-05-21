19cd8c431bb7f4ac31adea7b5ce37a07259b2fdb
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-21 04:58:42 +0300

    added a testclass for image

--- insta/tests.py
+++ insta/tests.py
@@ -1,5 +1,18 @@
 from django.test import TestCase
-from .models import Image,Profile
+from .models import Image,Profile,User
 
 # Create your tests here.
-class 
+class ImageTestClass(TestCase):
+    def setup(self):
+        self.user = User.objects.create_user(username = 'user')
+        self.image = Image(name = "name",caption = "me")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+
+    def test_save_method(self):
+        self.image.save_image()
+        image = Image.objects.all()
+        self.assertTrue(len(image) > 0)
+
